a363b4d19516348becf8e7dc0779eb32c69f97e3
Coffee_machine <sapvilte@gmail.com>
2023-09-11 21:48:41 +0300

    created draft menu (#2)
    Co-authored-by: Vilte Sapož <Sapvilte@gmail.com>
    Reviewed-on: https://git.xe1h.xyz/xE1H/sizzling-plate/pulls/2
    Reviewed-by: xE1H <me@xe1h.xyz>

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+menu_dict = json.load(open("menu.json"))
 
 
 @app.route('/')
@@ -10,7 +12,7 @@ def homepage():
 
 @app.route('/menu')
 def menu():
-    return render_template("menu.html")
+    return render_template("menu.html", menu=menu_dict)
 
 
 if __name__ == '__main__':
