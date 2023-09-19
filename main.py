e1d2bb68b082277d15da18b50893213385b1155a
CafeAuLaitMachine <sapvilte@gmail.com>
2023-09-19 21:48:42 +0300

    Created checkout and confirmation page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import json
 
 app = Flask(__name__)
@@ -10,9 +10,31 @@ def homepage():
     return render_template("index.html")
 
 
-@app.route('/menu')
+@app.route('/menu', methods=["POST", "GET"])
 def menu():
     return render_template("menu.html", menu=menu_dict)
+
+
+@app.route('/checkout/<string:dish>', methods=["POST", "GET"])
+@app.route('/checkout')
+def checkout(dish=None, menu=menu_dict):
+    for food in menu:
+        if food['item'] == dish:
+            chosen_food = food
+    if request.method == 'POST':
+        client = dict()
+        client["name"] = request.form['nm']
+        client["surname"] = request.form['srn']
+        client["gmail"] = request.form['gmail']
+        client["order"] = chosen_food
+        return redirect('/confirmation/' + json.dumps(client))
+    else:
+        return render_template("checkout.html", dish=chosen_food)
+
+
+@app.route('/confirmation/<string:client>')
+def confirm(client):
+    return render_template("con_page.html", client=json.loads(client))
 
 
 if __name__ == '__main__':
